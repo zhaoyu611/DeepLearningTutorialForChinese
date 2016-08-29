ee3b051c0ae84cae3d12061ada978e4e38328c50
zhaoyu <zhaoyuafeu@gmail.com>
2016-08-29 19:55:33 +0800

    finish rbm.py with bug

--- rbm.py
+++ rbm.py
@@ -9,6 +9,7 @@ __author__ = 'Administrator'
 import cPickle
 import gzip
 import PIL.Image
+import time
 
 import numpy
 
@@ -27,7 +28,7 @@ class RBM(object):
         """
         RBM类定义了从隐层到可见层(反之亦然)的模型参数和基本操作，
         同时定义了CD的更新
-        :param input:对于标准RBM，输入为None，对于大模型的RBM模块，输入为符号变量
+        :param input:对于标准RBM，输入为None，对于大模型的RBM模块，输入为符号变量,取值区间[0,1]
         :param n_visible:可见单元的数量
         :param n_hidden:隐藏单元的数量
         :param W:对于标准RBM,输入为None；对于DBN网络的RBM模块，输入为共享权重矩阵的符号变量；
@@ -97,13 +98,18 @@ class RBM(object):
     #定义向上传播
     def propup(self,vis):
         '''
-        定义从可见单元到隐藏单元的传播函数。注意函数的返回值是未sigmoid运算的值。
+        定义从可见单元到隐藏单元的传播函数。注意函数的返回值是未sigmoid运算的值,区间[0,1]。
         As it will turn out later, due to how Theano deals with
         optimizations, this symbolic variable will be needed to write
         down a more stable computational graph (see details in the
         reconstruction cost function)
         vis: 可见层单元
         '''
+        #input:
+        #       vis: 可见单元的值,区间[0,1]，theano.shared(dtype=thenao.config.floatX)
+        #output:
+        #       pre_sigmoid_activation: 未sigmoid运算的值,区间[0,1]
+        #       T.nnet.sigmoid(pre_sigmoid_activation)：经过sigmoid运算的值,区间[0,1]
         pre_sigmoid_activation=T.dot(vis,self.W)+self.hbias
         return [pre_sigmoid_activation,T.nnet.sigmoid(pre_sigmoid_activation)]
     #给定v单元计算h单元的函数
@@ -112,6 +118,12 @@ class RBM(object):
         #利用激活函数获得隐层样本
         #注意theano_rng.binomial返回dtype为int64的符号变量。
         #如果想在GPU上进行计算，需要将类型转换为floatX
+        #input:
+        #       v0_sample: 区间[0,1] theano.shared(dtype=thenao.config.floatX)
+        #output:
+        #       pre_sigmoid_h1: 加权求和的值 theano.shared(dtype=thenao.config.floatX)
+        #       h1_mean: 经过sigmoid运算的值 theano.shared(dtype=thenao.config.floatX)
+        #       h1_sample:二值化的值 theano.shared(dtype=thenao.config.floatX),{0,1}
         h1_sample=self.theano_rng.binomial(size=h1_mean.shape,
                                            n=1,p=h1_mean,
                                            dtype=theano.config.floatX)
@@ -125,15 +137,24 @@ class RBM(object):
         optimizations, this symbolic variable will be needed to write
         down a more stable computational graph (see details in the
         reconstruction cost function)
-
-        hid:隐层单元
         '''
+        #input:
+        #       hid: 隐藏单元的值  区间[0,1]，
+        #output：
+        #       pre_sigmoid_activation:未sigmoid运算的值,区间[0,1],theano.shared(dtype=thenao.config.floatX)
+        #       T.nnet.sigmoid(pre_sigmoid_activation): 经过sigmoid值,区间[0,1]
         pre_sigmoid_activation=T.dot(hid,self.W.T)+self.vbias
         return [pre_sigmoid_activation,T.nnet.sigmoid(pre_sigmoid_activation)]
 
 
     #给定h单元计算v单元的函数
     def sample_v_given_h(self,h0_sample):
+        #input:
+        #       h0_sample: 隐藏单元的值  区间[0,1]
+        #output:
+        #       pre_sigmoid_v1:未sigmoid运算的值,区间[0,1]
+        #       v1_mean: 经过sigmoid值,区间[0,1]
+        #       v1_sample: 二值化的值，取值{0,1}
         pre_sigmoid_v1,v1_mean=self.propdown(h0_sample)
         v1_sample=self.theano_rng.binomial(size=v1_mean.shape,n=1,p=v1_mean,
                                            dtype=theano.config.floatX)
@@ -141,19 +162,29 @@ class RBM(object):
 
     #从隐藏状态出发，执行一步Gibbs采样过程
     def gibbs_hvh(self,h0_sample):
+        #input:
+        #       h0_sample: 隐藏单元的值  区间[0,1]
+        #output：
+        #       pre_sigmoid_v1:未sigmoid运算的值,区间[0,1]
+        #       v1_mean:经过sigmoid值,区间[0,1]
+        #       v1_sample:二值化的值，取值{0,1}
+        #       pre_sigmoid_h1:未sigmoid运算的值,区间[0,1]
+        #       h1_mean:经过sigmoid值,区间[0,1]
+        #       h1_sample:二值化的值，取值{0,1}
         pre_sigmoid_v1,v1_mean,v1_sample=self.sample_v_given_h(h0_sample)
-        pre_sigmoid_h1,h1_mean,h1_sample=self.sample_v_given_h(v1_sample)
+        pre_sigmoid_h1,h1_mean,h1_sample=self.sample_h_given_v(v1_sample)
         return [pre_sigmoid_v1,v1_mean,v1_sample,
                 pre_sigmoid_h1,h1_mean,h1_sample]
 
     #从可见状态出发，执行一步Gibbs采样过程
     def gibbs_vhv(self,v0_sample):
         pre_sigmoid_h1,h1_mean,h1_sample=self.sample_h_given_v(v0_sample)
-        pre_sigmoid_v1,v1_mean,v1_sample=self.sample_h_given_v(h1_sample)
+        pre_sigmoid_v1,v1_mean,v1_sample=self.sample_v_given_h(h1_sample)
         return [pre_sigmoid_h1,h1_mean,h1_sample,
                 pre_sigmoid_v1,v1_mean,v1_sample]
 
     def get_cost_updates(self,lr=0.1,persistent=None,k=1):
+
         """
         函数执行一步CD-k或者PCD-k
         :param lr: 训练RBM的学习率
@@ -208,11 +239,11 @@ class RBM(object):
         return monitoring_cost,updates
 
     #伪似然函数的随机近似算法
-    def get_pesudo_likehood_cost(self,updates):
+    def get_pseudo_likehood_cost(self,updates):
         #定义表达式p{x_i|x{\i}}的索引i
         bit_i_idx=theano.shared(value=0,name='bit_i_idx')
         #二值化输入图像，将它近似到最近的整数
-        xi=T.round(self.input)  #?????????input已经是二值化了，为什么还要round？
+        xi=T.round(self.input)
         #给定bit设置后计算自由能
         fe_xi=self.free_energy(xi)
         #在矩阵xi中反转变量x_i，并保存其他变量x_{\i}。
@@ -230,18 +261,136 @@ class RBM(object):
         """近似重构误差算法
         注意函数要求未sigmoid激活的值作为输入量。如果想深入了解这样做的原因，那么
         需要了解Theano的工作原理。当编译Theano函数时，计算图中输入量的速度和稳定性
-        得到优化，这是通过改变子图中若干部分实现的。这样的优化代表softplus中log(sigmoid(x))项。对于交叉熵，当sigmoid值大于30(结果趋于1)，
-        就需要这样的优化。当sigmoid值小于-30(结果趋于0)，则Theano计算log(0)，最终代价为-inf
+        得到优化，这是通过改变子图中若干部分实现的。这样的优化代表softplus中log(sigmoid(x))项。
+        对于交叉熵，当sigmoid值大于30(结果趋于1)，就需要这样的优化。
+        当sigmoid值小于-30(结果趋于0)，则Theano计算log(0)，最终代价为-inf
         或者NaN。通常情况下，softplus中log(sigmoid(x))项会得到正常值。但这里遇到特殊情况：
         sigmoid在scan优化内部，log在外部。因此，Theano会执行log(scan(…))而不是log(sigmoid(…))，
         也不会进行优化。我们找不到替代scan中sigmoid的方法，因为只需要在最后一步执行。最简单有效
         的办法是输出未sigmoid的值，在scan之外同时应用log和sigmoid。
-
-        :param updates:
-        :param pre_sigmoid_nv:
-        :return:
         """
+        cross_entropy=T.mean(T.sum(self.input*T.log(T.nnet.sigmoid(pre_sigmoid_nv))+
+                                   (1-self.input)*T.log(1-T.nnet.sigmoid(pre_sigmoid_nv))),axis=1)
+
+        return cross_entropy
+
+def test_rbm(learning_rate=0.1,training_epochs=15,
+             dataset='./data/mnist.pkl.gz',batch_size=20,
+             n_chains=20,n_samples=10,out_folder='rbm_plots',
+             n_hidden=500):
+    """
+    使用Theano训练和采样的函数,测试集合：MNIST
+    :param learning_rate:  训练RBM的学习率
+    :param training_epochs: 训练的迭代次数
+    :param dataset: 数据集的路径
+    :param batch_size: 训练RBM的块大小
+    :param n_chains: 用于采样的并行Gibbs链数
+    :param n_samples: 每条链中需要画出的样本数
+    :param out_folder: 输出图的保存路径
+    :param n_hidden: 隐层单元数量
+    """
+    datasets=load_data(dataset)
+    train_set_x,train_set_y=datasets[0]
+    test_set_x,test_set_y=datasets[2]
+    #计算用于训练、验证和测试的minibatch的数量
+    n_train_batches=train_set_x.get_value(borrow=True).shape[0]/batch_size
+    #定义数据的符号函数
+    index=T.lscalar() #minibatch的索引
+    x=T.matrix('x') #栅格化的图像数据
+
+    rng=numpy.random.RandomState(123)
+    theano_rng=RandomStreams(rng.randint(2**30))
+
+    #初始化固定链
+    persistent_chain=theano.shared(numpy.zeros((batch_size,n_hidden),
+                                               dtype=theano.config.floatX),
+                                   borrow=True)
+
+    #构造RBM类
+    rbm=RBM(input=x,n_visible=28*28,n_hidden=n_hidden,numpy_rng=rng,theano_rng=theano_rng)
+
+    #获得一步CD-15的代价和梯度
+    cost,updates=rbm.get_cost_updates(lr=learning_rate,persistent=persistent_chain,k=15)
+
+    #################################
+    #          RBM的训练过程         #
+    #################################
+
+    #设置输出文件的路径
+    if not os.path.isdir(out_folder):
+        os.makedirs(out_folder)
+    os.chdir(out_folder)
+
+    #theano函数可以没有输出量，train_rbm实现RBM参数更新的功能
+    train_rbm=theano.function([index],cost,updates=updates,
+                              givens={x:train_set_x[index*batch_size:(index+1)*batch_size]},
+                              name='train_rbm')
+    plotting_time=0.
+    start_time=time.clock()
+
+    #进行训练迭代
+    for epoch in xrange(training_epochs):
+        #遍历训练集合
+        mean_cost=[]
+        for batch_index in xrange(n_train_batches):
+            mean_cost+=[train_rbm(batch_index)]
+
+        print "training epoch %d, cost is %f" %(epoch,numpy.mean(mean_cost))
+
+        #每个训练epoch画出filers
+        plotting_start=time.clock()
+        #画出权重矩阵的可视图
+        image=PIL.Image.fromarray(tile_raster_images(
+            X=rbm.W.get_vale(borrow=True).T,
+            img_shape=(28,28),tile_shape=(10,10),
+            tile_spacing=(1,1)))
+        image.save('filters_at_epoch_%i.png'%epoch)
+        plotting_stop=time.clock()
+        plotting_time+=plotting_stop-plotting_start
+    end_time=time.clock()
+    pretrain_time=(end_time-start_time)-plotting_time
+    print "Training took %f minutes"%(pretrain_time/60.)
+
+    #################################
+    #          RBM的采样过程         #
+    #################################
+    #计算测试样本数量
+    number_of_test_samples=test_set_x.get_value(borrow=True).shape[0]
+
+    #随机选择用于初始化固定链的测试样本
+    test_idx=rng.randint(number_of_test_samples-n_chains)
+    persistent_vis_chain=theano.shared(numpy.asarray(
+                test_set_x.get_value(borrow=True)[test_idx:test_idx+n_chains]),
+                dtype=theano.config.floatX)
+
+    plot_every=1000
+    #定义一步Gibbs采样(mf=mean-field) 中，每隔plot_every步才进行一次作图
+    [presig_hids,hid_mfs,hid_samples,
+     presig_vis,vis_mfs,vis_samples],updates=\
+                                    theano.scan(rbm.gibbs_vhv,
+                                                outputs_info=[None,None,None,None,None,persistent_vis_chain],
+                                                n_steps=plot_every)
+    #增加固定链的共享变量到updates字典
+    updates.update({persistent_vis_chain:vis_samples[-1]})
+    #构造函数执行persistent_chain
+    #生成""mean field"用于画图，真实样本数据初始化固定链
+    sample_fn=theano.function([],[vis_mfs[-1],vis_samples[-1]],
+                              updates=updates,name='sample_fn')
+    #开辟存储图片的空间，用于画图(同样需要为tile_spacing预留空间)
+    image_data=numpy.zeros((29*n_samples+1,29*n_chains-1),dtype='uint8')
+    for idx in xrange(n_samples):
+        #每'plot_every'次画一次图，丢弃中间量，因为链中样本相关性太强
+        vis_mf,vis_sample=sample_fn()
+        print '...plotting sample', idx
+        image_data[29*idx:29*idx+28,:]=tile_raster_images(
+            X=vis_mf,img_shape=(28,28),tile_shape=(1,n_chains),
+            tile_spacing=(1,1))
+        # 重构图像
+    image=PIL.Image.fromarray(image_data)
+    image.save('sample.png')
+    os.chdir('../')
+
 
 #测试函数
 if __name__=='__main__':
-    rbm=RBM()
+    test_rbm()
